97a846644d7ee40ecc02eea777dea48029e4b4de
fritskeboogie <fritjof.herrstrom@gmail.com>
2026-05-19 15:07:04 +0200

    chore: update dev server host

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const { buildApp } = require("./server/app");
 const port = Number(process.env.PORT || 8000);
 const app = buildApp();
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Parranda listening on http://localhost:${port}`);
 });
