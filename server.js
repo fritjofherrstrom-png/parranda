6be633e0263bd8bd5163ed92a4d46bb2d9dc6cee
fritskeboogie <fritjof.herrstrom@gmail.com>
2026-05-20 14:29:46 +0200

    chore(dev): print LAN preview URLs

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
+const os = require("node:os");
+
 const { buildApp } = require("./server/app");
 
 const port = Number(process.env.PORT || 8000);
+const host = process.env.HOST || "0.0.0.0";
 const app = buildApp();
 
-app.listen(port, "0.0.0.0", () => {
+function getLocalNetworkUrls(portNumber) {
+  return Object.values(os.networkInterfaces())
+    .flat()
+    .filter((networkInterface) =>
+      networkInterface &&
+      networkInterface.family === "IPv4" &&
+      !networkInterface.internal,
+    )
+    .map((networkInterface) => `http://${networkInterface.address}:${portNumber}`);
+}
+
+app.listen(port, host, () => {
   console.log(`Parranda listening on http://localhost:${port}`);
+
+  if (host === "0.0.0.0" || host === "::") {
+    const networkUrls = getLocalNetworkUrls(port);
+
+    if (networkUrls.length) {
+      console.log("Open on a phone on the same Wi-Fi:");
+      networkUrls.forEach((url) => console.log(`  ${url}`));
+    } else {
+      console.log("No LAN address found. Make sure Wi-Fi is on if you want mobile preview.");
+    }
+  } else {
+    console.log(`Bound to host ${host}`);
+  }
 });
